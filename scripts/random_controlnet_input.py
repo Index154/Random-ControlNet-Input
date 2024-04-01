9d0c4344d1d37c746c981cdcabdfe85c883c04e0
artificialIndex <105119758+Index154@users.noreply.github.com>
2024-04-01 19:39:09 +0200

    Batch count support

--- scripts/random_controlnet_input.py
+++ scripts/random_controlnet_input.py
@@ -1,10 +1,11 @@
 import gradio as gr
 import os
 import modules.scripts as scripts
-from modules.processing import process_images, Processed
+from modules.processing import process_images, process_images_inner, Processed
 from modules.shared import state
 
 import random
+import copy
 from PIL import Image
 from io import BytesIO
 import base64
@@ -15,7 +16,7 @@ import glob
 class Script(scripts.Script):
 
     def title(self):
-        return "Random ControlNet Input"
+        return "Random ControlNet Input Batch"
 
     def ui(self, is_img2img):
         uiActive = gr.Checkbox(True, label="Activate script")
@@ -38,29 +39,86 @@ class Script(scripts.Script):
                     parts = replace[1:-1].split('=>')
                     text = text.replace(parts[0], '=>'.join(parts[1:]))
             return text
-            
-        # Function for txt file reading
-        def readTxt(txtPath):
-            try:
-                with open(txtPath, 'r') as txtFile:
-                    txtContent = txtFile.read().replace('\n', '')
-            except:
-                raise Exception("<Random Controlnet Input> - Failed to read text file " + txtPath)
-            return txtContent
         
-        # Function for loading default.txt
-        def getDefault(txtPath):
-            splitTxtPath = txtPath.split(separator)
-            mainDefaultPath = os.path.join(uiFolderPath, 'default.txt')
+        # Function for loading a random image
+        def generateImage(p):
+            p2 = copy.copy(p)
             
-            loaded = ''
-            index = -1
-            while index < len(splitTxtPath) and loaded == '':
-                path = os.path.join(separator.join(splitTxtPath[:index]), "default.txt")
-                if os.path.isfile(path): loaded = readTxt(path)
-                if path == mainDefaultPath : break
-                index -= 1
-            return loaded
+            # Function for txt file reading
+            def readTxt(txtPath):
+                try:
+                    with open(txtPath, 'r') as txtFile:
+                        txtContent = txtFile.read().replace('\n', '')
+                except:
+                    raise Exception("<Random Controlnet Input> - Failed to read text file " + txtPath)
+                return txtContent
+            
+            # Function for loading default.txt
+            def getDefault(txtPath):
+                splitTxtPath = txtPath.split(separator)
+                mainDefaultPath = os.path.join(uiFolderPath, 'default.txt')
+                
+                loaded = ''
+                index = -1
+                while index < len(splitTxtPath) and loaded == '':
+                    path = os.path.join(separator.join(splitTxtPath[:index]), "default.txt")
+                    if os.path.isfile(path): loaded = readTxt(path)
+                    if path == mainDefaultPath : break
+                    index -= 1
+                return loaded
+            
+            # Enable ControlNet if turned off
+            controlNetList = controlNetModule.get_all_units_in_processing(p2)
+            if(not controlNetList[0].enabled):
+                if uiForceControlNet : controlNetList[0].enabled = True
+                else: return
+        
+            # Select a random image
+            roll = random.randrange(1, totalWeight + 1)
+            selectedImg = ''
+            weightCheck = 0
+            i = 0
+            while i < len(folders) and selectedImg == '':
+                weightCheck += folders[i]['value']
+                if(roll <= weightCheck):
+                    if (len(folders[i]['images']) > 0):
+                        selectedImg = random.choice(folders[i]['images'])
+                i += 1
+            if selectedImg == '' : raise Exception("<Random Controlnet Input> - The randomization ended without a chosen image even though other checks should have prevented this from happening. Please test the reproducability of this and report the issue")
+            
+            # Load image
+            try:
+                with Image.open(selectedImg) as img:
+                    if uiFlip and random.choice([True, False]) : img = img.transpose(Image.FLIP_LEFT_RIGHT)
+                    io = BytesIO()
+                    img.save(io, format='PNG')
+                    imgData = controlNetModule.to_base64_nparray(base64.b64encode(io.getvalue()).decode())
+            except:
+                raise Exception("<Random Controlnet Input> - Failed to read image file " + selectedImg)
+                return
+            
+            # Add text from txt file with same filename to prompt if enabled. If no file is found, look for default.txt in the same folder and in the main folder entered by the user
+            if(uiModifyPrompt):
+                txtPath = selectedImg[:-4] + '.txt'
+                loadedTxt = ''
+                if(os.path.isfile(txtPath)):
+                    loadedTxt = readTxt(txtPath)
+                else:
+                    loadedTxt = getDefault(txtPath)
+                    
+                if(loadedTxt != ''):
+                    p2.prompt += ", "
+                    if uiIgnorePrompt or batch_count > 1 : p2.prompt = ''
+                    p2.prompt += loadedTxt
+                    if '{default}' in p2.prompt : p2.prompt = p2.prompt.replace('{default}', getDefault(txtPath))  # Replace {default} with contents from default.txt
+                    p2.prompt = replaceText(p2.prompt)
+            
+            # Change the controlnet input image to the selected one
+            controlNetList[0].image = imgData
+            controlNetModule.update_cn_script_in_processing(p2, controlNetList)
+            proc = process_images(p2)
+            return proc
+        
     
         # Get substitution strings from prompt and remove them
         pattern = r'!.*?=>.*?!'
@@ -87,8 +145,8 @@ class Script(scripts.Script):
             p.prompt = replaceText(p.prompt)
             proc = process_images(p)
             return proc
-            
-        # Error if ControlNet is missing
+        
+        # Error if controlnet is missing
         try:
             controlNetModule = importlib.import_module('extensions.sd-webui-controlnet.scripts.external_code', 'external_code')
         except:
@@ -103,12 +161,6 @@ class Script(scripts.Script):
         # Error if folder does not exist
         if(not os.path.isdir(uiFolderPath)):
             raise Exception("<Random ControlNet Input> - Image source folder not found")
-        
-        # Enable ControlNet if turned off
-        controlNetList = controlNetModule.get_all_units_in_processing(p)
-        if(not controlNetList[0].enabled):
-            if uiForceControlNet : controlNetList[0].enabled = True
-            else: return
         
         # Get list of files in folder
         if(uiRecursive):
@@ -156,50 +208,26 @@ class Script(scripts.Script):
             if len(f['images']) < 1 : raise Exception("<Random Controlnet Input> - The custom weight for \"" + f['name'] + "\" is affecting no images! Please check for typos")
         print('')
         
-        # Select a random image
-        roll = random.randrange(1, totalWeight + 1)
-        selectedImg = ''
-        weightCheck = 0
-        i = 0
-        while i < len(folders) and selectedImg == '':
-            weightCheck += folders[i]['value']
-            if(roll <= weightCheck):
-                if (len(folders[i]['images']) > 0):
-                    selectedImg = random.choice(folders[i]['images'])
-            i += 1
-        if selectedImg == '' : raise Exception("<Random Controlnet Input> - The randomization ended without a chosen image even though other checks should have prevented this from happening. Please test the reproducability of this and report the issue")
-        
-        # Load image
-        try:
-            with Image.open(selectedImg) as img:
-                if uiFlip and random.choice([True, False]) : img = img.transpose(Image.FLIP_LEFT_RIGHT)
-                io = BytesIO()
-                img.save(io, format='PNG')
-                imgData = controlNetModule.to_base64_nparray(base64.b64encode(io.getvalue()).decode())
-        except:
-            raise Exception("<Random Controlnet Input> - Failed to read image file " + selectedImg)
-            return
-        
-        # Add text from txt file with same filename to prompt if enabled. If no file is found, look for default.txt in the same folder and in the main folder entered by the user
-        if(uiModifyPrompt):
-            txtPath = selectedImg[:-4] + '.txt'
-            loadedTxt = ''
-            if(os.path.isfile(txtPath)):
-                loadedTxt = readTxt(txtPath)
-            else:
-                loadedTxt = getDefault(txtPath)
-                
-            if(loadedTxt != ''):
-                p.prompt += ", "
-                if uiIgnorePrompt : p.prompt = ''
-                p.prompt += loadedTxt
-                if '{default}' in p.prompt : p.prompt = p.prompt.replace('{default}', getDefault(txtPath))  # Replace {default} with contents from default.txt
-                p.prompt = replaceText(p.prompt)
-        
-        # Change the controlnet input image to the selected one
-        controlNetList[0].image = imgData
-        controlNetModule.update_cn_script_in_processing(p, controlNetList)
-        
         # Begin generating
-        proc = process_images(p)
-        return proc
+        batch_results = None
+        batch_count = p.n_iter
+        p.n_iter = 1
+        print(f"Will process {batch_count} images, creating {p.batch_size} new images for each.")
+        state.job_count = batch_count
+        for i in range(batch_count):
+            state.job = f"{i+1} out of {batch_count}"
+            if state.skipped:
+                state.skipped = False
+
+            if state.interrupted or state.stopping_generation:
+                break
+            proc = generateImage(p)
+            
+            if proc is not None:
+                if batch_results:
+                    batch_results.images.extend(proc.images)
+                    batch_results.infotexts.extend(proc.infotexts)
+                else:
+                    batch_results = proc
+            
+        return batch_results
